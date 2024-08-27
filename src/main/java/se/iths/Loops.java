5a7373514f0e0b12f03fdfa9477349d27059654d
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-27 10:48:27 +0200

    Add second example with while

--- src/main/java/se/iths/Loops.java
+++ src/main/java/se/iths/Loops.java
@@ -7,21 +7,30 @@ public class Loops {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        whileLoop();
+        whileLoop2();
+    }
 
+    private static void whileLoop2() {
+        int n = 1;
+        while (n > 0) {
+            System.out.println("Hej");
+            //Ask user for input
+            System.out.print("Write 1 to continue or 0 to exit: ");
+            n = scanner.nextInt();
+        }
     }
 
     private static void whileLoop() {
-        while( true ) {
+        while (true) {
             System.out.println("Hej");
             //Ask user for input
             System.out.print("Write 1 to continue or 0 to exit: ");
             int n = scanner.nextInt();
-            if( n == 0 ) {
+            if (n == 0) {
                 break;
                 //return;
             }
-                System.out.println("I see that you want to continue");
+            System.out.println("I see that you want to continue");
         }
     }
 }
