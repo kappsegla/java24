dc7f9b4fe0e2b9802ab4c945d03868639577dc45
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-27 13:12:10 +0200

    Read and write one number

--- src/main/java/se/iths/ArrayExample.java
+++ src/main/java/se/iths/ArrayExample.java
@@ -1,14 +1,23 @@
 package se.iths;
 
+import java.util.Scanner;
+
 public class ArrayExample {
+    //Read one number from user and print it
+    //Todo: Read two numbers from user and print them
+    //Todo: Read 10 numbers from user and print them
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        System.out.print("Write a number: ");
+        int i = scanner.nextInt();
+        System.out.println("You wrote " + i);
 
-        //Read one number from user and print it
 
-        //Read two numbers from user and print them
-
-        //Read 10 numbers from user and print them
 
 
     }
+
+
+
 }
