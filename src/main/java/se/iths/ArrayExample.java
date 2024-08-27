3733bd371e9095086c2abd2cfc32b23c0fc5ccef
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-27 13:42:28 +0200

    Read 10 numbers and print

--- src/main/java/se/iths/ArrayExample.java
+++ src/main/java/se/iths/ArrayExample.java
@@ -4,20 +4,25 @@ import java.util.Scanner;
 
 public class ArrayExample {
     //Read one number from user and print it
-    //Todo: Read two numbers from user and print them
+    //Read two numbers from user and print them
     //Todo: Read 10 numbers from user and print them
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Write a number: ");
-        int numberOne = scanner.nextInt();
-        System.out.print("Write a number: ");
-        int numberTwo = scanner.nextInt();
+        int[] numbers = new int[10];
 
-        System.out.println("You wrote " + numberOne + " " + numberTwo);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Write a number: ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        System.out.print("You wrote ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(numbers[i] + " ");
+        }
 
     }
-
 
 
 }
