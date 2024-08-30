911558e050aefe3314341d343d7167537c22a151
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-30 14:14:56 +0200

    Code cleanup

--- src/main/java/se/iths/exercise/Exercise6.java
+++ src/main/java/se/iths/exercise/Exercise6.java
@@ -11,23 +11,21 @@ public class Exercise6 {
         int[] reverseArray = new int[ARRAY_SIZE];
 
         Random rand = new Random();
-        for (int i = 0; i < ARRAY_SIZE ; i++) {
+        for (int i = 0; i < ARRAY_SIZE; i++) {
             //anArray[i] = (int) (Math.random() * 11);
             int randomNumber = rand.nextInt(10);
             anArray[i] = randomNumber;
         }
         int j = ARRAY_SIZE - 1;
-        for (int i = 0; i < ARRAY_SIZE ; i++) {
+        for (int i = 0; i < ARRAY_SIZE; i++) {
             reverseArray[j] = anArray[i];
             j--;
         }
         System.out.println(Arrays.toString(anArray));
         System.out.println(Arrays.toString(reverseArray));
         int sum = 0;
-        for (int i = 0; i < anArray.length; i+=2) {
-            //if((i % 2) == 0) {
-                sum += anArray[i];
-            //}
+        for (int i = 0; i < anArray.length; i += 2) {
+            if ((i % 2) == 0) sum += anArray[i];
         }
         System.out.println(sum);
     }
