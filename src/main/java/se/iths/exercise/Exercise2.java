cc186d6482af7f3489297f9a21dc6438efe9150c
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-30 10:29:33 +0200

    Add check for negative values and same value

--- src/main/java/se/iths/exercise/Exercise2.java
+++ src/main/java/se/iths/exercise/Exercise2.java
@@ -4,16 +4,26 @@ import java.util.Scanner;
 
 public class Exercise2 {
     public static Scanner scnr = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
         System.out.println("Ange två siffror: ");
-        int num1= scnr.nextInt();
-        int num2= scnr.nextInt();
+        int num1 = scnr.nextInt();
+        int num2 = scnr.nextInt();
 
         double med = (num1 + num2) / 2.;
-        System.out.println("Medelvärdet är: " + med );
-    
+        System.out.println("Medelvärdet är: " + med);
 
-
+        if (num1 < 0) {
+            num1 = num1 * -1;
+        }
+        if (num2 < 0) {
+            num2 = num2 * -1;
+        }
+        if (num1 == num2) {
+            System.out.println("Summan av de två talen gånger två är: " + (num1 + num2) * 2);
+        } else {()
+            System.out.println("Summan av de två talen är: " + (num1 + num2));
+        }
     }
 }
