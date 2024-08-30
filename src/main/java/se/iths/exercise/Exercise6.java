ce3226c4d629cabaf0ef348aa27da206b7bfbf6c
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-30 13:52:08 +0200

    Calculate sum of numbers stored at even indexes

--- src/main/java/se/iths/exercise/Exercise6.java
+++ src/main/java/se/iths/exercise/Exercise6.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Exercise6 {
-    public static final int ARRAY_SIZE = 20;
+    public static final int ARRAY_SIZE = 10;
 
     public static void main(String[] args) {
         int[] anArray = new int[ARRAY_SIZE];
@@ -23,6 +23,13 @@ public class Exercise6 {
         }
         System.out.println(Arrays.toString(anArray));
         System.out.println(Arrays.toString(reverseArray));
+        int sum = 0;
+        for (int i = 0; i < anArray.length; i+=2) {
+            //if((i % 2) == 0) {
+                sum += anArray[i];
+            //}
+        }
+        System.out.println(sum);
     }
 
 }
