9d2b26bb7ae09ccff05b24c0bfc990c1605390d6
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-30 09:36:13 +0200

    Read name and print

--- src/main/java/se/iths/exercise/Exercise1.java
+++ src/main/java/se/iths/exercise/Exercise1.java
@@ -1,4 +1,17 @@
 package se.iths.exercise;
 
+import java.util.Scanner;
+
 public class Exercise1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Vad heter du? ");
+        String namn = sc.nextLine();
+
+        String 
+        System.out.println("Hej " + namn);
+
+    }
 }
