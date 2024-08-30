f91bd34564c0590feeca2fe15aa844671bc7741a
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-30 09:42:12 +0200

    Add capitalization of first letter

--- src/main/java/se/iths/exercise/Exercise1.java
+++ src/main/java/se/iths/exercise/Exercise1.java
@@ -8,10 +8,11 @@ public class Exercise1 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Vad heter du? ");
-        String namn = sc.nextLine();
+        String name = sc.nextLine();
 
-        String 
-        System.out.println("Hej " + namn);
+        String s = name.substring(0, 1).toUpperCase();
+        String nameCapitilized = s + name.substring(1);
+        System.out.println("Hej " + nameCapitilized);
 
     }
 }
