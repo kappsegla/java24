7fc5e46c6616fc8f004d730db245489af777f372
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-30 14:53:12 +0200

    Refactoring of code

--- src/main/java/se/iths/exercise/Exercise8.java
+++ src/main/java/se/iths/exercise/Exercise8.java
@@ -8,31 +8,20 @@ public class Exercise8 {
         //1,2,1,3,4,5   Somewhere
         //1,2,3,4,5,6   Nowhere
 
-        boolean Everywhere = true;
-        boolean Somewhere = false;
+        boolean everywhere = true;
+        boolean somewhere = false;
 
         for (int i = 0; i < numbers.length - 3; i += 2) {
 
             if (numbers[i] == numbers[i + 2] || numbers[i + 1] == numbers[i + 3] ||
                 numbers[i] == numbers[i + 3] || numbers[i + 1] == numbers[i + 2]) {
-                Somewhere = true;
+                somewhere = true;
             } else {
-                Everywhere = false;
+                everywhere = false;
             }
-
-
         }
-        if (Everywhere) {
-            System.out.println("everywhere");
-        }else if (Somewhere) {
-            System.out.println("somewhere");
-        }else {
-            System.out.println("nowhere");
-        }
-
-
-
-
-
+        if (everywhere) System.out.println("everywhere");
+        else if (somewhere) System.out.println("somewhere");
+        else System.out.println("nowhere");
     }
 }
