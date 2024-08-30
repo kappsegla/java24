b88e8d235defcdfdee3a5e0b06df612b124b7dbb
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-30 11:09:36 +0200

    Add enhanced switch

---
+++ src/main/java/se/iths/exercise/Exercise4.java
@@ -0,0 +1,26 @@
+package se.iths.exercise;
+import java.util.Scanner;
+public class Exercise4 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("What day of the week is it? ");
+        String dayOfWeek = scanner.nextLine().toLowerCase();
+
+        int dayNumber = switch (dayOfWeek) {
+            case "monday", "mon" -> 1;
+            case "tuesday", "tue" -> 2;
+            case "wednesday", "wed" -> 3;
+            case "thursday", "thu" -> 4;
+            case "friday", "fri" -> 5;
+            case "saturday", "sat" -> 6;
+            case "sunday", "sun" -> 7;
+            default -> 0;
+            // Koden nedan avslutar programmet och ger ett error
+            // default -> throw new IllegalArgumentException();
+        };
+
+        System.out.println("Ditt nummer är: " + dayNumber);
+    }
+
+}
