d113a4af4e523f14e3660d02a8e5813b8e11d089
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-30 13:24:17 +0200

    Fill array with random numbers

--- src/main/java/se/iths/exercise/Exercise6.java
+++ src/main/java/se/iths/exercise/Exercise6.java
@@ -1,6 +1,20 @@
 package se.iths.exercise;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Exercise6 {
 
+    public static void main(String[] args) {
+        int[] anArray = new int[10];
+        // Math.random()
+        Random rand = new Random();
+        for (int i = 0; i < 10 ; i++) {
+            //anArray[i] = (int) (Math.random() * 11);
+            int randomNumber = rand.nextInt(10);
+            anArray[i] = randomNumber;
+        }
+        System.out.println(Arrays.toString(anArray));
+    }
 
 }
