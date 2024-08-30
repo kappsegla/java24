e7929a88a18d105b2eb409e2823e5608084fb5ef
Martin Blomberg <martin.blomberg@outlook.com>
2024-08-30 13:31:31 +0200

    Copy array in revers order

--- src/main/java/se/iths/exercise/Exercise6.java
+++ src/main/java/se/iths/exercise/Exercise6.java
@@ -7,14 +7,21 @@ public class Exercise6 {
 
     public static void main(String[] args) {
         int[] anArray = new int[10];
-        // Math.random()
+        int[] reverseArray = new int[10];
+
         Random rand = new Random();
         for (int i = 0; i < 10 ; i++) {
             //anArray[i] = (int) (Math.random() * 11);
             int randomNumber = rand.nextInt(10);
             anArray[i] = randomNumber;
         }
+        int j = 9;
+        for (int i = 0; i <anArray.length ; i++) {
+            reverseArray[j] = anArray[i];
+            j--;
+        }
         System.out.println(Arrays.toString(anArray));
+        System.out.println(Arrays.toString(reverseArray));
     }
 
 }
