02bd1fc0aaa9a0c8cf5606adde38376aca6c0c5a
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-03 13:16:43 +0200

    Tax Calculator

---
+++ src/main/java/se/iths/method/TaxCalculator.java
@@ -0,0 +1,15 @@
+package se.iths.method;
+
+public class TaxCalculator {
+    public static void main(String[] args) {
+        System.out.println(calculateTax(110000));
+        System.out.println(calculateTax(90000));
+    }
+    public static float calculateTax(int income){
+        if(income > 100_000){
+            return (income - 100_000) * 0.4f + 30_000;
+        }
+        //throw new IllegalStateException();
+        return income * 0.3f;
+    }
+}
