a0c964d0d2d62cb26a1f53fbddd8d204c1738390
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-04 13:02:39 +0200

    Add methods to Circle

--- src/main/java/se/iths/oop/Circle.java
+++ src/main/java/se/iths/oop/Circle.java
@@ -1,6 +1,24 @@
 package se.iths.oop;
 
 public class Circle {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    //circumference, omkretsen på runda föremål circle, oval
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
 
 
 }
