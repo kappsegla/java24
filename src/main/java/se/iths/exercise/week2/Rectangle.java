372beed5ecf086b75a844e4f72dc112fd5ebf10d
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-06 13:24:21 +0200

    Add methods and constructor to Rectangle

--- src/main/java/se/iths/exercise/week2/Rectangle.java
+++ src/main/java/se/iths/exercise/week2/Rectangle.java
@@ -4,32 +4,39 @@ public class Rectangle {
     private float width;
     private float height;
 
-    public Rectangle(float width, float height){
+    public Rectangle(float width, float height) {
+        if (width <= 0 || height <= 0)
+            throw new IllegalArgumentException("width and height must be greater than 0");
         this.width = width;
         this.height = height;
     }
-    
-    public float getWidth(){
+
+    public float getWidth() {
         return width;
     }
-    public void setWidth(float width){
+
+    public void setWidth(float width) {
         this.width = width;
     }
-    public float getHeight(){
+
+    public float getHeight() {
         return height;
     }
-    public void setHeight(float height){
+
+    public void setHeight(float height) {
         this.height = height;
     }
-    public float getArea(){
+
+    public float getArea() {
         return width * height;
     }
-    public float getPerimeter(){
+
+    public float getPerimeter() {
         return 2 * (width + height);
     }
 
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(5.f , 10.f);
+        Rectangle rectangle = new Rectangle(5.f, 10.f);
         System.out.println("Area: " + rectangle.getArea());
         System.out.println("Omkrets: " + rectangle.getPerimeter());
     }
