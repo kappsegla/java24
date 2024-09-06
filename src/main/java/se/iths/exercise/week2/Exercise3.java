05864b4e795ab2ef234b1b7a96d5c1a12759698e
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-06 11:23:10 +0200

    Palindrome with for loop

--- src/main/java/se/iths/exercise/week2/Exercise3.java
+++ src/main/java/se/iths/exercise/week2/Exercise3.java
@@ -1,18 +1,26 @@
 package se.iths.exercise.week2;
 
 public class Exercise3 {
-
     public static void main(String[] args) {
-        System.out.println(isPalindrome("boolean"));
-        System.out.println(isPalindrome("racecar"));
-        System.out.println(isPalindrome("level"));
-        System.out.println(isPalindrome("saippuakivikauppias"));
-        System.out.println(isPalindrome("1221"));
-        System.out.println(isPalindrome("Tattarrattat"));
+        System.out.println(isPalindrome("boolean")); //false
+        System.out.println(isPalindrome("racecar"));//True
+        System.out.println(isPalindrome("level")); // true
+        System.out.println(isPalindrome("saippuakivikauppias")); // true
+        System.out.println(isPalindrome("1221")); // True
+        System.out.println(isPalindrome("Tattarrattat")); // Ska vara true (men retunerar false för att den jämför T med t)
     }
 
     public static boolean isPalindrome(String input) {
-        return false;
-    }
+        //Converts input to lowercase to be case INSENSITIVE
+        input = input.toLowerCase();
 
+        //Loop through all characters in string
+        for (int i = 0; i < input.length(); i++) {
+            // Compares character at current index with it's reversed index
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
