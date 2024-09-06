dd728699400495bf795a6bb5aa02fa33971bdf76
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-06 09:22:10 +0200

    Find minimum number

--- src/main/java/se/iths/exercise/week2/Exercise1.java
+++ src/main/java/se/iths/exercise/week2/Exercise1.java
@@ -1,4 +1,30 @@
 package se.iths.exercise.week2;
 
+
+import java.util.Scanner;
+
 public class Exercise1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Skriv in första numret: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Skriv in andra numret: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Skriv in tredje numret: ");
+        int num3 = scanner.nextInt();
+
+        System.out.println("Det minsta numret är : " + findMinimum(num1, num2, num3));
+
+
+    }
+
+    public static int findMinimum(int a, int b, int c) {
+        int min = Math.min(a, b);
+        min = Math.min(min, c);
+        return min;
+    }
+
 }
