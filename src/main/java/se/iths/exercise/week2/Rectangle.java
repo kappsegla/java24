f5f1b8412e36bad4285ad7c703e8ef7b7f2fa907
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-06 13:17:31 +0200

    Rectangle class

--- src/main/java/se/iths/exercise/week2/Rectangle.java
+++ src/main/java/se/iths/exercise/week2/Rectangle.java
@@ -1,4 +1,36 @@
 package se.iths.exercise.week2;
 
 public class Rectangle {
+    private float width;
+    private float height;
+
+    public Rectangle(float width, float height){
+        this.width = width;
+        this.height = height;
+    }
+    
+    public float getWidth(){
+        return width;
+    }
+    public void setWidth(float width){
+        this.width = width;
+    }
+    public float getHeight(){
+        return height;
+    }
+    public void setHeight(float height){
+        this.height = height;
+    }
+    public float getArea(){
+        return width * height;
+    }
+    public float getPerimeter(){
+        return 2 * (width + height);
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5.f , 10.f);
+        System.out.println("Area: " + rectangle.getArea());
+        System.out.println("Omkrets: " + rectangle.getPerimeter());
+    }
 }
