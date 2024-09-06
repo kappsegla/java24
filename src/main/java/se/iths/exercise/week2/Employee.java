f0d97310356e72e0acdb2b446c14717f49b90196
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-06 14:46:38 +0200

    Calculate employment time

--- src/main/java/se/iths/exercise/week2/Employee.java
+++ src/main/java/se/iths/exercise/week2/Employee.java
@@ -1,6 +1,8 @@
 package se.iths.exercise.week2;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 
 
 public class Employee {
@@ -23,7 +25,6 @@ public class Employee {
     public int getSalary() {
         return salary;
     }
-    
 
     public void setSalary(int salary) {
         this.salary = salary;
@@ -31,6 +32,36 @@ public class Employee {
 
     public LocalDate getStartDate() {
         return startDate;
+    }
+
+    //Use Period and Period.between for calculating an answer in years, months, days
+    public int employmentTime(LocalDate date) {
+        return (int) startDate.until(date, ChronoUnit.DAYS);
+    }
+
+    public int employmentTime() {
+        return employmentTime(LocalDate.now());
+    }
+
+    public int employmentTime(String date) {
+        try {
+            return employmentTime(LocalDate.parse(date));
+        } catch (DateTimeParseException e) {
+            // Error message should be moved to class that implements this function
+            System.out.println("Not correct formatted date!");
+            throw e;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Employee employee = new Employee("John Smith", 1000, LocalDate.of(1990, 1, 1));
+        System.out.println(employee.employmentTime(LocalDate.now()));
+        System.out.println(employee.employmentTime("2021-03-31"));
+
+        System.out.println(employee.getSalary()); // Should print 1000
+        employee.setSalary(2000);
+        System.out.println(employee.getSalary()); // Should print 2000
     }
 
 }
