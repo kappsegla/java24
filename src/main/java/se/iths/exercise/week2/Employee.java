9edeabb8c4df835a36a4df4a751accc807593e24
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-06 13:44:14 +0200

    Add employee class

--- src/main/java/se/iths/exercise/week2/Employee.java
+++ src/main/java/se/iths/exercise/week2/Employee.java
@@ -1,4 +1,18 @@
 package se.iths.exercise.week2;
 
+import java.time.LocalDate;
+
+
 public class Employee {
+    private String name;
+    private int salary;
+    private LocalDate startDate;
+
+    public Employee(String name, int salary, LocalDate startDate){
+        if (name.isBlank())
+            throw new IllegalArgumentException("Name cannot be empty");
+        this.name = name;
+        this.salary = salary;
+        this.startDate = startDate;
+    }
 }
