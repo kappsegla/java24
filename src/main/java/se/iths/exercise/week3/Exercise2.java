392f764d2ccbe729de381f5b1276475e2f3d78bf
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-10 10:42:41 +0200

    Exercise2 add overloaded exclusion method.

--- src/main/java/se/iths/exercise/week3/Exercise2.java
+++ src/main/java/se/iths/exercise/week3/Exercise2.java
@@ -1,16 +1,36 @@
 package se.iths.exercise.week3;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Exercise2 {
     public static void main(String[] args) {
-        String[] inArray = {"1", "2", "3", "4"};
+        String[] inArray = {"1", "2", "3", "4","2"};
+
         System.out.println(Arrays.toString(copyWithExclusion(inArray, 0)));
         System.out.println(Arrays.toString(copyWithExclusion(inArray, 1)));
         System.out.println(Arrays.toString(copyWithExclusion(inArray, 2)));
         System.out.println(Arrays.toString(copyWithExclusion(inArray, 3)));
         System.out.println(Arrays.toString(copyWithExclusion(inArray, 4))); // Ska returnera inArray
         System.out.println(Arrays.toString(copyWithExclusion(inArray, -1))); // Ska returnera inArray
+        System.out.println(Arrays.toString(copyWithExclusion(inArray, "2")));
+    }
+
+    public static String[] copyWithExclusion(String[] org, String exclusionString) {
+        int lost = 0;
+        for (String s : org) {
+            if (Objects.equals(s, exclusionString)) {
+                lost++;
+            }
+        }
+        String[] tempArray = new String[org.length - lost];
+        int j = 0;
+        for (String s: org){
+            if (!Objects.equals(s, exclusionString)){
+                tempArray[j++] = s;
+            }
+        }
+        return tempArray;
     }
 
     public static String[] copyWithExclusion(String[] org, int exclusionIndex) {
