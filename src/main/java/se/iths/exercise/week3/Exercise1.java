8199fdff710a9cf378138e678a62a62b3e1c9f2e
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-10 09:22:16 +0200

    Exercise1 week3

--- src/main/java/se/iths/exercise/week3/Exercise1.java
+++ src/main/java/se/iths/exercise/week3/Exercise1.java
@@ -1,4 +1,22 @@
 package se.iths.exercise.week3;
 
 public class Exercise1 {
+
+    public static void main(String[] args) {
+
+        int [] number = {3, 4, 5};
+
+        //number[0] = 74846;
+        //number[1] = 56;
+        //number[2] = 4766;
+
+        evenOrOdd(number);
+    }
+    public static void evenOrOdd(int[] input){
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 != 0){
+                throw new RuntimeException("Det finns udda siffror!");
+            }
+        }
+    }
 }
