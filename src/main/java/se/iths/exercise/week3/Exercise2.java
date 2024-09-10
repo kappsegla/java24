81f2b1ee6cfcf56275d55b45da89bc97d2d24152
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-10 09:48:28 +0200

    Add validation

--- src/main/java/se/iths/exercise/week3/Exercise2.java
+++ src/main/java/se/iths/exercise/week3/Exercise2.java
@@ -6,10 +6,18 @@ public class Exercise2 {
     public static void main(String[] args) {
         String[] inArray = {"1", "2", "3", "4"};
         System.out.println(Arrays.toString(copyWithExclusion(inArray, 0)));
+        System.out.println(Arrays.toString(copyWithExclusion(inArray, 1)));
+        System.out.println(Arrays.toString(copyWithExclusion(inArray, 2)));
+        System.out.println(Arrays.toString(copyWithExclusion(inArray, 3)));
         System.out.println(Arrays.toString(copyWithExclusion(inArray, 4))); // Ska returnera inArray
+        System.out.println(Arrays.toString(copyWithExclusion(inArray, -1))); // Ska returnera inArray
     }
 
     public static String[] copyWithExclusion(String[] org, int exclusionIndex) {
+
+        if (exclusionIndex >= org.length || exclusionIndex < 0 || org == null || org.length == 0) {
+            return org;
+        }
 
         String[] tempArray = new String[org.length - 1];
         int j = 0;
