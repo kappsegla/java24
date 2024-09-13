b9b433c20aba8d59475125ff19e6eaab2e34a49c
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-13 10:16:03 +0200

    Add MemberLedger

---
+++ src/main/java/se/iths/oop/person/MemberLedger.java
@@ -0,0 +1,9 @@
+package se.iths.oop.person;
+
+public class MemberLedger {
+
+    public static void main(String[] args) {
+        MemberLedger m = new MemberLedger();
+
+    }
+}
--- src/main/java/se/iths/oop/person/Person.java
+++ src/main/java/se/iths/oop/person/Person.java
@@ -1,6 +1,12 @@
 package se.iths.oop.person;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 //Immutable object
-public record Person(String name, LocalDate birthDate) {}
+public record Person(String name, LocalDate birthDate) {
+
+    public int age() {
+        return (int) ChronoUnit.YEARS.between(birthDate, LocalDate.now());
+    }
+}
