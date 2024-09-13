056ed42173b047aa69090ee66a2bec1b6202f240
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-13 10:43:09 +0200

    Add fee method

--- src/main/java/se/iths/oop/person/MemberLedger.java
+++ src/main/java/se/iths/oop/person/MemberLedger.java
@@ -1,9 +1,28 @@
 package se.iths.oop.person;
 
+import java.time.LocalDate;
+
 public class MemberLedger {
+    private Person[] persons = new Person[3];
 
     public static void main(String[] args) {
         MemberLedger m = new MemberLedger();
+        m.persons[0] = new Person("Donald", LocalDate.of(1996, 2, 29));
+        m.persons[1] = new Person("Stella", LocalDate.of(2001, 7, 2));
+        m.persons[2] = new Person("Kalle", LocalDate.of(1963, 1, 1));
 
+        for (Person p : m.persons) {
+            System.out.println(p.name() + " " + p.age() + " " + m.fee(p));
+        }
+    }
+
+    public int fee(Person p) {
+        if( p.name().equalsIgnoreCase("Kalle") ) {
+            return 200;
+        }
+        if( p.age() < 25 ) {
+            return 200;
+        }
+        return 350;
     }
 }
