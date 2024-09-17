f13f1dd01d3fdc5caf635c7443e8b9ef3063ba44
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-17 10:41:05 +0200

    anonymous classes for implementing interface

--- src/main/java/se/iths/oop/lambda/SAMInterface.java
+++ src/main/java/se/iths/oop/lambda/SAMInterface.java
@@ -15,5 +15,15 @@ class SamDemo {
     public static void main(String[] args) {
         SAMInterface sam = new SamImplementation();
         sam.sayHi();
+
+        //Anonymous class, might be called SamDemo$1
+        SAMInterface sam2 = new SAMInterface() {
+            @Override
+            public void sayHi() {
+                System.out.println(this.getClass().getName());
+            }
+        };
+        sam2.sayHi();
+
     }
 }
