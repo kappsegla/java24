a6480a343be04b1829ff6e79370469bee5cb256a
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-17 11:19:10 +0200

    Use extracted method instead of multiline lambdas

--- src/main/java/se/iths/oop/lambda/SAMInterface.java
+++ src/main/java/se/iths/oop/lambda/SAMInterface.java
@@ -1,5 +1,6 @@
 package se.iths.oop.lambda;
 
+@FunctionalInterface
 public interface SAMInterface {
     void sayHi();
 }
@@ -29,5 +30,14 @@ class SamDemo {
         SAMInterface sam3 = () -> System.out.println("Hi from lambda");
         sam3.sayHi();
 
+        //Lambda implementation of a single abstract method interface
+        SAMInterface sam4 = () -> printGreeting();
+        sam4.sayHi();
+
+    }
+
+    private static void printGreeting() {
+        System.out.println("Hi from lambda");
+        System.out.println("Second line of code");
     }
 }
