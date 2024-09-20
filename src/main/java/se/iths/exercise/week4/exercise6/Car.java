7fcba4f6be07473eb93952c9d22f04cadae49a7e
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-20 14:25:45 +0200

    Update Car and Truck

--- src/main/java/se/iths/exercise/week4/exercise6/Car.java
+++ src/main/java/se/iths/exercise/week4/exercise6/Car.java
@@ -1,8 +1,14 @@
 package se.iths.exercise.week4.exercise6;
 
-public class Car extends Vehicle{
+public class Car extends Vehicle {
+    private int numberOfSeats;
 
-    public Car(String registrationNumber){
+    public Car(String registrationNumber, int numberOfSeats) {
         super(registrationNumber);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
     }
 }
--- src/main/java/se/iths/exercise/week4/exercise6/Truck.java
+++ src/main/java/se/iths/exercise/week4/exercise6/Truck.java
@@ -1,8 +1,14 @@
 package se.iths.exercise.week4.exercise6;
 
 public class Truck extends Vehicle {
+    private final int loadCapacity;
 
-    public Truck(String registrationNumber) {
+    public Truck(String registrationNumber, int loadCapacity) {
         super(registrationNumber);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public int getLoadCapacity() {
+        return loadCapacity;
     }
 }
