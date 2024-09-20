0873556997399c2a86e1a45b95473b7e426e2dd4
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-20 09:54:47 +0200

    Implement the rest

---
+++ src/main/java/se/iths/exercise/week4/BaseBall.java
@@ -0,0 +1,19 @@
+package se.iths.exercise.week4;
+
+public class BaseBall extends Ball {
+
+
+    public BaseBall(String brandName) {
+        super(brandName);
+    }
+
+    @Override
+    public void toss(){
+
+    }
+    @Override
+    public void bounce(){
+
+    }
+
+}
---
+++ src/main/java/se/iths/exercise/week4/Football.java
@@ -0,0 +1,19 @@
+package se.iths.exercise.week4;
+
+public class Football extends Ball{
+
+    public Football(String brandName) {
+        super(brandName);
+    }
+
+    @Override
+    public void bounce() {
+
+    }
+
+
+    @Override
+    public void toss() {
+
+    }
+}
