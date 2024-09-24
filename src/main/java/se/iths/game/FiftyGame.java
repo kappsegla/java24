e357d8835b9562e10089832a7171478476d19130
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-24 13:47:10 +0200

    Change from ArrayList to List

--- src/main/java/se/iths/game/FiftyGame.java
+++ src/main/java/se/iths/game/FiftyGame.java
@@ -1,5 +1,7 @@
 package se.iths.game;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -17,16 +19,18 @@ public class FiftyGame {
 
     private void play() {
         //Slumpa value 1-50
-        int value = 30;// rand.nextInt(RAND_BOUND);
+        int value = rand.nextInt(RAND_BOUND);
         int guessCount = 0;
         boolean exit = false;
+        List<String> guesses = new ArrayList<>();
+
         while (!exit) {
             System.out.print("Make a guess between 1 and " + MAX_RANDOM + ": ");
             int guess = sc.nextInt();
             guessCount++;
+            guesses.add(String.valueOf(guess));
             if (guess == value) {
-                System.out.println("Congratulations! You win after " + guessCount +
-                                   (guessCount > 1 ? " guesses.": " guess."));
+                victoryCelebration(guessCount, guesses);
                 exit = true;
             } else if (guess < value) {
                 System.out.println("Your guess is too low!");
@@ -35,5 +39,13 @@ public class FiftyGame {
             }
         }
         System.out.println("Thanks for playing");
+    }
+
+    private static void victoryCelebration(int guessCount, List<String> guesses) {
+        System.out.println("Congratulations! You win after " + guessCount +
+                           (guessCount > 1 ? " guesses.": " guess."));
+        System.out.print("Your" + (guessCount > 1 ? " guesses ": " guess ") +
+                         "are ");
+        System.out.println(String.join(",", guesses));
     }
 }
