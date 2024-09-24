1216d83100a4fa84166dfa2e4171753db2f97a00
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-24 13:03:17 +0200

    Ternary operator

--- src/main/java/se/iths/QuizExample.java
+++ src/main/java/se/iths/QuizExample.java
@@ -4,6 +4,6 @@ public class QuizExample {
     public static final boolean FEATURE = false;
 
     public static void main(String[ ] args) {
-        int i = 0;
+
     }
 }
--- src/main/java/se/iths/game/FiftyGame.java
+++ src/main/java/se/iths/game/FiftyGame.java
@@ -17,13 +17,16 @@ public class FiftyGame {
 
     private void play() {
         //Slumpa value 1-50
-        int value = rand.nextInt(RAND_BOUND);
+        int value = 30;// rand.nextInt(RAND_BOUND);
+        int guessCount = 0;
         boolean exit = false;
         while (!exit) {
             System.out.print("Make a guess between 1 and " + MAX_RANDOM + ": ");
             int guess = sc.nextInt();
+            guessCount++;
             if (guess == value) {
-                System.out.println("Congratulations! You win!");
+                System.out.println("Congratulations! You win after " + guessCount +
+                                   (guessCount > 1 ? " guesses.": " guess."));
                 exit = true;
             } else if (guess < value) {
                 System.out.println("Your guess is too low!");
