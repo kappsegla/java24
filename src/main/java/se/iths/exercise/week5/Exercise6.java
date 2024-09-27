d26c4288ffe91ae0c7b8f9a392896f55213bf358
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-27 13:00:40 +0200

    Add quotes

--- src/main/java/se/iths/exercise/week5/Exercise6.java
+++ src/main/java/se/iths/exercise/week5/Exercise6.java
@@ -1,8 +1,33 @@
 package se.iths.exercise.week5;
 
+import java.util.*;
+
+import static se.iths.exercise.week5.Exercise4.createSetFromString;
+
 public class Exercise6 {
 
+    public static Map<String, Set<Integer>> index = new HashMap<>();
+
     public static void main(String[] args) {
+        List<String> quotes = new ArrayList<>();
+
+        quotes.add("Why do Java developers wear glasses? Because they can't C#!");
+        quotes.add("Why did the Java developer go broke? Because he lost his JavaBeans!");
+        quotes.add("How do you spot a Java programmer at a party? They're the ones constantly throwing exceptions!");
+        quotes.add("Why did the Java developer bring a ladder to the office? He heard the coffee was on the Java roof!");
+        quotes.add("What do you call a haunted Java programming language? A Java ghost script!");
+        quotes.add("Why do programmers prefer dark mode? Because light attracts bugs!");
+        quotes.add("Why was the JavaScript developer sad? Because he didn't know how to 'null' his feelings.");
+        quotes.add("Why do Python programmers have low self-esteem? Because they're constantly comparing themselves to others.");
+        quotes.add("Why did the developer go broke? Because he used up all his cache.");
+        quotes.add("Why do programmers hate nature? It has too many bugs.");
+
+        for (int i = 0; i < quotes.size(); i++) {
+            Set<String> words = createSetFromString(quotes.get(i));
+
+        }
+
+
 
     }
 }
