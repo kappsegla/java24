b7d3a06419ac0d62cc4a5f714ababf54fa8321da
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-27 11:13:00 +0200

    Inline method calls

--- src/main/java/se/iths/exercise/week5/Exercise4.java
+++ src/main/java/se/iths/exercise/week5/Exercise4.java
@@ -7,8 +7,7 @@ import java.util.Set;
 public class Exercise4 {
 
     public static Set<String> createSetFromString(String str){
-        String[] words = str.toLowerCase().split(" ");
-        return new HashSet<>(List.of(words));
+        return new HashSet<>(List.of(str.toLowerCase().split(" ")));
     }
 
     public static void main(String[] args) {
