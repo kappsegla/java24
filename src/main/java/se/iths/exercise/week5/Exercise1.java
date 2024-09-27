5b90467f8dc20f0e5333a2815b3f7da749403958
Martin Blomberg <martin.blomberg@outlook.com>
2024-09-27 09:13:24 +0200

    Exercise1

---
+++ src/main/java/se/iths/exercise/week5/Exercise1.java
@@ -0,0 +1,33 @@
+package se.iths.exercise.week5;
+
+import java.util.ArrayList;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        // 1. Creating an ArrayList
+        ArrayList<String> list = new ArrayList<>();
+        // 2. Adding elements to the ArrayList
+        list.add("Coffee");
+        list.add("Tea");
+        // 3. Accessing an element in the ArrayList
+        String index0 = list.get(0);
+        String index1 = list.get(1);
+        String first = list.getFirst();
+        String last = list.getLast();
+        // 4. Modifying an element in the ArrayList
+
+
+
+
+    }
+
+
+
+
+// 5. Removing an element from the ArrayList
+// 6. Checking if the ArrayList is empty
+// 7. Getting the size of the ArrayList
+// 8. Checks if an object exists in the ArrayList.
+// 9. Write a custom sorting of the objects in the ArrayList with an implementation of Comparator .
+}
