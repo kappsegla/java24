3c0b0a815ddaaf6ff530e313c4952283e8b498bc
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-04 10:10:52 +0200

    Refactor employees to field

--- src/main/java/se/iths/exercise/week6/App.java
+++ src/main/java/se/iths/exercise/week6/App.java
@@ -3,27 +3,30 @@ package se.iths.exercise.week6;
 import java.util.List;
 
 public class App {
+    public final static List<Employee> employees = EmployeeFactory.getAllEmployees();
 
     public static void main(String[] args) {
 
         //allEmployeesWithSalaryOver(2_000_000);
-        numberOfEmployees();
+        //numberOfEmployees();
+        employeeWithHighestSalary();
 
     }
 
     public static void allEmployeesWithSalaryOver(int amount) {
-        List<Employee> employees = EmployeeFactory.getAllEmployees();
         employees.stream()
                 .filter(employee -> employee.salary() > amount)
                 .forEach(System.out::println);
     }
 
     public static void numberOfEmployees() {
-        List<Employee> employees = EmployeeFactory.getAllEmployees();
-
         var count = employees.stream().count();
 
         System.out.println(count);
+    }
+
+    public static void employeeWithHighestSalary() {
+
     }
 
 
