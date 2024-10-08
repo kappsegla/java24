daf311fdf734876057ede080fa8b09c9883f6467
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-08 14:48:42 +0200

    Map to PersonAttendance

--- src/main/java/se/iths/io/Attendance.java
+++ src/main/java/se/iths/io/Attendance.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.stream.Collectors;
 
 public class Attendance {
@@ -32,12 +34,40 @@ public class Attendance {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        //Find attendance time for each person in minutes
+        try {
+            try (var lines = Files.lines(attendanceFile, StandardCharsets.UTF_16LE)) {
+                //Find all unique names that has attended
+                var persons = lines.skip(1)
+                        .map(line -> line.split("\t"))
+                        .map(PersonAttendance::of)
+                        .collect(Collectors.toSet());
+                persons.forEach(System.out::println);
 
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        //Hur många unika personer har närvarat
 
         //Hur länge har varje person varit ansluten
 
 
     }
+}
+
+record PersonAttendance(String name, ConnectionStatus status, LocalDateTime dateTime) {
+    public static PersonAttendance of(String[] data) {
+        ConnectionStatus status = ConnectionStatus.DISCONNECTED;
+        if (data[1].equals("Ansluten"))
+            status = ConnectionStatus.CONNECTED;
+
+        return new PersonAttendance(data[0].replaceAll(" - .*$", ""),
+                status,
+                LocalDateTime.parse(data[2], DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss")));
+    }
+}
+
+enum ConnectionStatus {
+    CONNECTED, DISCONNECTED
 }
