f96773ede162dd4d0a1e0c1f4df390f24d323fce
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-08 14:08:48 +0200

    Find all distinct names

--- src/main/java/se/iths/io/Attendance.java
+++ src/main/java/se/iths/io/Attendance.java
@@ -12,16 +12,24 @@ public class Attendance {
         Path attendanceFile = Path.of("meetingAttendanceList.csv");
         if (Files.exists(attendanceFile)) {
             System.out.println("Found attendance file");
-        }
-        else {
+        } else {
             System.out.println("Attendance file not found");
             return;
         }
 
         //Läsa alla rader från filen
+
         try {
-            var lines = Files.readAllLines(attendanceFile, StandardCharsets.UTF_16LE);
-            lines.forEach(System.out::println);
+            var lines = Files.lines(attendanceFile, StandardCharsets.UTF_16LE);
+
+            lines.skip(1)
+                    .map(line -> line.split("\t")[0])
+                    .map(name -> name.substring(0, !name.contains(" - ") ? name.length() : name.indexOf(" - ")))
+                    //.map(name-> name.replaceAll(" - .*$", ""))
+                    .distinct()
+                    .forEach(System.out::println);
+
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -31,10 +39,6 @@ public class Attendance {
         //Hur många unika personer har närvarat
 
         //Hur länge har varje person varit ansluten
-
-
-
-
 
 
     }
