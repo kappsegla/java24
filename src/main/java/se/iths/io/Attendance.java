a4e4ef1e5b0986c954837c038b3a31007f8c3574
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-08 13:07:42 +0200

    Check if csv file exists in working directory

--- src/main/java/se/iths/io/Attendance.java
+++ src/main/java/se/iths/io/Attendance.java
@@ -1,11 +1,24 @@
 package se.iths.io;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Attendance {
     public static void main(String[] args) {
 
         //Kolla om attendancefilen finns
+        Path attendanceFile = Path.of("meetingAttendanceList.csv");
+        if (Files.exists(attendanceFile)) {
+            System.out.println("Found attendance file");
+        }
+        else {
+            System.out.println("Attendance file not found");
+            return;
+        }
 
         //Läsa alla rader från filen
+
+
 
         //Hitta alla unika namn som närvarat
 
