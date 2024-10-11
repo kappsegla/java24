f0968682e205d0d8d555c9b5b9297bc5530ad105
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-11 11:51:48 +0200

    Works great

--- src/main/java/se/iths/exercise/week7/either/Demo.java
+++ src/main/java/se/iths/exercise/week7/either/Demo.java
@@ -3,17 +3,31 @@ package se.iths.exercise.week7.either;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Demo {
     public static void main(String[] args) {
         var demo = new Demo();
 
-        var userIds = List.of(0,1,2,3);
+        var userIds = List.of(0, 1, 2, 3);
 
-            userIds.stream().map(demo::getUserAccount)
-                    .filter(Either::isRight)
-                    .map(Either::getRight)
-                    .forEach(System.out::println);
+        //Keep only found accounts and ignore errors by filtering out left Either objects
+        userIds.stream().map(demo::getUserAccount)
+                .filter(Either::isRight)
+                .map(Either::getRight)
+                .forEach(System.out::println);
+
+        //Keep both errors (left) and accounts ( right ) in one list for each
+        //Avoids doing the streaming and calls to getUserAccount twice to get everything.
+        var result = userIds.stream()
+                .map(demo::getUserAccount)
+                .collect(Collectors.teeing(
+                        Collectors.filtering(Either::isLeft, Collectors.mapping(Either::getLeft, Collectors.toList())),
+                        Collectors.filtering(Either::isRight, Collectors.mapping(Either::getRight, Collectors.toList())),
+                        List::of));
+
+        System.out.println("Errors: " + result.get(0));
+        System.out.println("Accounts: " + result.get(1));
 
 //        for( var userId : userIds ) {
 //            Either either = demo.getUserAccount(userId);
@@ -31,7 +45,7 @@ public class Demo {
     public Either getUserAccount(int userId) {
         //Kolla om det finns en användare med id userId
         var user = getUserById(userId);
-        if( user.isEmpty())
+        if (user.isEmpty())
             return Either.left(new Error.UserNotFound());
         //Kolla om användaren har ett konto?
         var accounts = getUserAccounts(user.get());
@@ -44,7 +58,7 @@ public class Demo {
     private Optional<User> getUserById(int userId) {
         if (userId < 1)
             return Optional.empty();
-        if( userId == 1)
+        if (userId == 1)
             return Optional.of(new User(userId, "Donald"));
         return Optional.of(new User(userId, "Minny"));
     }
