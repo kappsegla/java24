dd3d6ef9594498870ecadd5234383bee3362db0c
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-11 13:18:20 +0200

    Changes by ViktorS

--- src/main/java/se/iths/exercise/week7/either/Demo.java
+++ src/main/java/se/iths/exercise/week7/either/Demo.java
@@ -12,9 +12,9 @@ public class Demo {
         var userIds = List.of(0, 1, 2, 3);
 
         //Keep only found accounts and ignore errors by filtering out left Either objects
-        userIds.stream().map(demo::getUserAccount)
-                .filter(Either::isRight)
-                .map(Either::getRight)
+        userIds.stream()
+                .map(demo::getUserAccount)
+                .filter(Either::isRight).map(Either::getRight)
                 .forEach(System.out::println);
 
         //Keep both errors (left) and accounts ( right ) in one list for each
@@ -42,41 +42,28 @@ public class Demo {
     //User har ett konto
     //UserId är giltigt
     //This method can return Either an Account or and Error... different types.
-    public Either getUserAccount(int userId) {
+    public Either<Error, Account> getUserAccount(int userId) {
         //Kolla om det finns en användare med id userId
         var user = getUserById(userId);
-        if (user.isEmpty())
-            return Either.left(new Error.UserNotFound());
+        if (user.isEmpty()) return Either.left(new Error.UserNotFound());
         //Kolla om användaren har ett konto?
         var accounts = getUserAccounts(user.get());
-        if (accounts.isEmpty())
-            return Either.left(new Error.AccountNotFound());
+        if (accounts.isEmpty()) return Either.left(new Error.AccountNotFound());
         return Either.right(accounts.stream().findFirst().get());
     }
 
     //This method can return zero or one User
     private Optional<User> getUserById(int userId) {
-        if (userId < 1)
-            return Optional.empty();
-        if (userId == 1)
-            return Optional.of(new User(userId, "Donald"));
+        if (userId < 1) return Optional.empty();
+        if (userId == 1) return Optional.of(new User(userId, "Donald"));
         return Optional.of(new User(userId, "Minny"));
     }
 
     //This method can return zero or many Accounts.
     //Can also use List if we can have duplicated values
     private Set<Account> getUserAccounts(User user) {
-        if (user == null)
-            return Set.of();
-        if (user.name().equals("Donald"))
-            return Set.of(new Account(1, user), new Account(3, user));
+        if (user == null) return Set.of();
+        if (user.name().equals("Donald")) return Set.of(new Account(1, user), new Account(3, user));
         return Set.of();
     }
-}
-
-record User(int id, String name) {
-}
-
-// a user can have zero or more accounts
-record Account(int id, User user) {
 }
